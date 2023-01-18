7d18a9b01e8b86cc808a3839dc7ac686022aee00
Junjie Chen <jjchen598@gmail.com>
2023-01-18 17:16:46 +0000

    Primeras pruebas

--- public/index.php
+++ public/index.php
@@ -1,1 +1,33 @@
-hi ifriendugo
+<?php
+require_once("../vendor/autoload.php");
+
+//Router system
+$router = new AltoRouter();
+
+//List of routes 
+
+$router->map('GET','/', function(){
+  echo 'Hola enrutamiento';
+});
+$router->map('GET','/cosa', function(){
+  echo 'Hola cosa';
+});
+//End of list 
+
+$match = $router->match();
+
+if ($match) {
+  $target = $match["target"];
+  if (is_string($target) && strpos($target, "#") !== false) {
+    list($controller, $action) = explode("#", $target);
+    $controller = "Controller\\" . $controller;
+    $controller = new $controller;
+    $controller->$action($match["params"]);
+  } else {
+    if (is_callable($match["target"])) call_user_func_array($match["target"], $match["params"]);
+    else require $match["target"];
+  }
+} else {
+  echo "Ruta no válida";
+  die();
+}
